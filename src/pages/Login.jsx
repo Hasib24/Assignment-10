6e770650d2c53570af09feb9d8df3da53e92d6d8
Hasib24 <hasibislam1488@gmail.com>
2023-05-02 01:53:06 +0600

    setup login and regester page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,13 +2,24 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Login = () => {
+
+    const handleOnSubmit = (e) =>{
+        e.preventDefault();
+        const form = e.target;
+        let email = form.email.value;
+        let password = form.password.value;
+
+        console.log(email, password);
+    }
+
+
     return (
         <div className='justify-center flex container mx-auto'>
-            <form className='p-10 border'>
+            <form className='p-10 border' onSubmit={handleOnSubmit}>
                 <input className='block border my-4 p-2 w-72' type="email" name="email" id="email" placeholder='Enter email' />
                 <input className='block border my-4 p-2 w-72' type="password" name="password" id="password" placeholder='Password' />
                 <input className='border w-72' type="submit" value="Login" />
-                <p className='my-5'>Don't have an account ? <Link to='/register'>Regester</Link> </p>
+                <p className='my-5'>Don't have an account ? <Link to='/register' className='text-blue-600'>Regester</Link> </p>
             </form>
         </div>
     );
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
+
+    const handleOnSubmit = (e) =>{
+        e.preventDefault();
+        const form = e.target;
+        let email = form.email.value;
+        let password = form.password.value;
+    }
+
+
+
     return (
-        <div>
-            register
+        <div className='justify-center flex container mx-auto'>
+            <form className='p-10 border' onSubmit={handleOnSubmit}>
+                <input className='block border my-4 p-2 w-72' type="text" name="name" id="name" placeholder='Enter name' />
+                <input className='block border my-4 p-2 w-72' type="email" name="email" id="email" placeholder='Enter email' />
+                <input className='block border my-4 p-2 w-72' type="password" name="password" id="password" placeholder='Password' />
+                <input className='border w-72' type="submit" value="Create" />
+                <p className='my-5'>Already have an account ? <Link to='/login' className='text-blue-600'>Login</Link> </p>
+            </form>
         </div>
     );
 };
