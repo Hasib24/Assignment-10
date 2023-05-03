a176e9c3b44a79dd09753cb7de8aec627a0c89a7
Hasib24 <hasibislam1488@gmail.com>
2023-05-03 19:38:21 +0600

    load chefCard data from api

--- src/pages/homepage_compo/ChefCard.jsx
+++ src/pages/homepage_compo/ChefCard.jsx
@@ -3,15 +3,17 @@ import React from 'react';
 import NormalBtn from '../../components/NormalBtn';
 import { FcLike } from 'react-icons/fc';
 
-const ChefCard = () => {
+const ChefCard = ({chef}) => {
+    
+    const {chefPicture, chefName, yearsOfExperience, numberOfRecipes, likes} = chef;
     return (
         <div className='mx-3 mb-5 p-5 border border-slate-300 rounded-md text-start flex flex-col justify-between shadow-md hover:shadow-xl hover:border-slate-400 duration-500'>
-            <img className='rounded-md' src="https://images.pexels.com/photos/3769999/pexels-photo-3769999.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
-            <h1 className='font-semibold'>Chef Name</h1>
-            <h1 className='font-semibold'>Experience : 5 years</h1>
-            <h1 className='font-semibold'>Recipes : 59834+</h1>
+            <img className='rounded-md h-48 object-cover' src={chefPicture} alt="" />
+            <h1 className='font-semibold'>Chef Name: {chefName}</h1>
+            <h1 className='font-semibold'>Experience : {yearsOfExperience} years</h1>
+            <h1 className='font-semibold'>Recipes : {numberOfRecipes}+</h1>
             <p className='flex justify-between items-center'>
-                <span>Likes</span>
+                <span>Likes: {likes}</span>
                 <NormalBtn>View Recipes</NormalBtn>
             </p>
         </div>
--- src/pages/homepage_compo/Chefs.jsx
+++ src/pages/homepage_compo/Chefs.jsx
@@ -1,25 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import BigBtn from '../../components/BigBtn';
 import ChefCard from './ChefCard';
+import { useLoaderData } from 'react-router-dom';
 
 const Chefs = () => {
-    return (
-        <div className='py-10 md:grid md:grid-cols-12 text-center'>
-            <div className='col-span-4'>
-                <img className='rounded-lg shadow-lg' src="https://images.pexels.com/photos/3814446/pexels-photo-3814446.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
-                <BigBtn>Master Chefs</BigBtn>
-            </div>
-            <div className='col-span-8 md:grid grid-cols-3'>
-                <ChefCard></ChefCard>
-                <ChefCard></ChefCard>
-                <ChefCard></ChefCard>
-                <ChefCard></ChefCard>
-                <ChefCard></ChefCard>
-                <ChefCard></ChefCard>
-            </div>
 
-            
+    let [chefs, setChefs] = useState(useLoaderData())
+
+    return (
+        <section className='bg-slate-100'>
+            <div className='container mx-auto py-10 md:grid md:grid-cols-12 text-center'>
+                <div className='col-span-4'>
+                    <img className='rounded-lg shadow-lg' src="https://images.pexels.com/photos/3814446/pexels-photo-3814446.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
+                    <BigBtn>Master Chefs</BigBtn>
+                </div>
+                <div className='col-span-8 md:grid grid-cols-3'>
+
+                    {chefs.map(chef => <ChefCard chef={chef} key={chef.id}></ChefCard>)}
+
+                </div>
         </div>
+        </section>
+        
     );
 };
 
