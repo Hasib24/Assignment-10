9c80c17c85968adcacaa2abdcf0e9033d5937b5f
Hasib24 <hasibislam1488@gmail.com>
2023-05-04 18:42:43 +0600

    implement lazy load to chefCard and chefRecipe card

--- src/pages/RecipeCard.jsx
+++ src/pages/RecipeCard.jsx
@@ -3,6 +3,8 @@ import NormalBtn from '../components/NormalBtn';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
+import LazyLoad from 'react-lazy-load';
+
 const RecipeCard = ({recipe}) => {
 
 
@@ -34,33 +36,36 @@ const RecipeCard = ({recipe}) => {
 
     const {id, recipiImg, recipiName, ingredients, cookingMethod, ratings} = recipe;
     return (
-        <div className='m-3 px-8 py-5 md:flex justify-between bg-white'>
-            <div className='border px-3 py-4'>
-                <h1>{recipiName}</h1>
-                <span>{ratings}</span>
-                <img className='w-60 rounded-md' src={recipiImg} alt="" />
-                
-            </div>
-            <div className='border px-3 py-4'>
-                <h1 className='text-xl font-semibold'>Ingradients:</h1>
-                <ul className='pl-4 list-disc'>
-                    {ingredients.map(ingra =><li key={ingra}>{ingra}</li>)}
-                </ul>
-            </div>
-            <div className='border px-3 py-4'>
-            <h1 className='text-xl font-semibold'>Coocking Method:</h1>
-                <ul className='pl-4 list-disc'>
-                    {cookingMethod.map(cook =><li key={cook}>{cook}</li>)}
-                </ul>
-            </div>
-            <div className='border flex items-center'>
-                <span onClick={()=>handleAddToFavourite(id)}><NormalBtn>Add to Favourite</NormalBtn></span>
-                
-            </div>
+        <LazyLoad threshold={0.95} offset={300}>
+            <div className='m-3 px-8 py-5 md:flex justify-between bg-white'>
+                <div className='border px-3 py-4'>
+                    <h1>{recipiName}</h1>
+                    <span>{ratings}</span>
+                    <img className='w-60 rounded-md' src={recipiImg} alt="" />
 
-            <ToastContainer></ToastContainer>
-            
-        </div>
+                </div>
+                <div className='border px-3 py-4'>
+                    <h1 className='text-xl font-semibold'>Ingradients:</h1>
+                    <ul className='pl-4 list-disc'>
+                        {ingredients.map(ingra =><li key={ingra}>{ingra}</li>)}
+                    </ul>
+                </div>
+                <div className='border px-3 py-4'>
+                <h1 className='text-xl font-semibold'>Coocking Method:</h1>
+                    <ul className='pl-4 list-disc'>
+                        {cookingMethod.map(cook =><li key={cook}>{cook}</li>)}
+                    </ul>
+                </div>
+                <div className='border flex items-center'>
+                    <span onClick={()=>handleAddToFavourite(id)}><NormalBtn>Add to Favourite</NormalBtn></span>
+
+                </div>
+
+                <ToastContainer></ToastContainer>
+
+            </div>
+        </LazyLoad>
+
     );
 };
 
--- src/pages/homepage_compo/ChefCard.jsx
+++ src/pages/homepage_compo/ChefCard.jsx
@@ -3,23 +3,27 @@ import React from 'react';
 import NormalBtn from '../../components/NormalBtn';
 import { FcLike } from 'react-icons/fc';
 import { Link } from 'react-router-dom';
+import LazyLoad from 'react-lazy-load';
 
 const ChefCard = ({chef}) => {
     
     const {id, chefPicture, chefName, yearsOfExperience, numberOfRecipes, likes} = chef;
     return (
-        <div className='mx-3 mb-5 p-5 border border-slate-300 rounded-md text-start flex flex-col justify-between shadow-md hover:shadow-xl hover:border-slate-400 duration-500'>
-            <img className='rounded-md h-48 object-cover' src={chefPicture} alt="" />
-            <h1 className='font-semibold'>Chef Name: {chefName}</h1>
-            <h1 className='font-semibold'>Experience : {yearsOfExperience} years</h1>
-            <h1 className='font-semibold'>Recipes : {numberOfRecipes}+</h1>
-            <p className='flex justify-between items-center'>
-                <span>Likes: {likes}</span>
-                
-                <Link to={`/chefrecipes/${id}`}><NormalBtn>View Recipes</NormalBtn></Link>
-                
-            </p>
-        </div>
+        <LazyLoad threshold={0.95}>   
+            <div className=' mx-3 mb-5 p-5 border border-slate-300 rounded-md text-start flex flex-col justify-between shadow-md hover:shadow-xl hover:border-slate-400 duration-500'>
+                <img className='rounded-md h-48 object-cover' src={chefPicture} alt="" />
+                <h1 className='font-semibold'>Chef Name: {chefName}</h1>
+                <h1 className='font-semibold'>Experience : {yearsOfExperience} years</h1>
+                <h1 className='font-semibold'>Recipes : {numberOfRecipes}+</h1>
+                <p className='flex justify-between items-center'>
+                    <span>Likes: {likes}</span>
+
+                    <Link to={`/chefrecipes/${id}`}><NormalBtn>View Recipes</NormalBtn></Link>
+
+                </p>
+            </div>
+        
+        </LazyLoad>
     );
 };
 
