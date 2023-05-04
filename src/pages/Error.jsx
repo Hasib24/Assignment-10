c552fbe038b6e6fd3d2f91e3fce82c0940178e27
Hasib24 <hasibislam1488@gmail.com>
2023-05-04 14:18:20 +0600

    add 404 error page design

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Error = () => {
     return (
-        <div>
-            400000000000000004 ERROR 
+        <div className=''>
+            <img className='h-[75vh] mx-auto' src="https://img.freepik.com/free-vector/oops-404-error-with-broken-robot-concept-illustration_114360-5529.jpg?w=740&t=st=1683188002~exp=1683188602~hmac=add6a4854e8024303d53a6cd4bfc01d95d4b358b9ab26cd5bcfa12dc071aceca" alt="" />
         </div>
     );
 };
