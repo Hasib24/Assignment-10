d3b776e9106c9eaa9d7764a6079baee80e738655
Hasib24 <hasibislam1488@gmail.com>
2023-05-05 14:13:57 +0600

    add regular expression to validate password

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -11,6 +11,9 @@ const auth = getAuth(app)
 
 const Register = () => {
 
+    let [ inputError, setInputError ] = useState('')
+    let [ inputSucess, setInputSucess ] = useState('')
+
     const { createUser, user, setUser } = useContext(AuthContext)
     const [tarmsAndPolices, setTarmAndPolicies] = useState(false)
 
@@ -22,6 +25,19 @@ const Register = () => {
         let password = form.password.value;
         let photoURL = form.photoURL.value;
 
+        if(!/(?=.*[a-z])/.test(password)){
+
+            setInputError('use alteast one lower case')
+            return
+        }else if(!/(?=.*[A-Z])/.test(password)){
+            setInputError('use at least one uppercase')
+            return
+        }
+        else if(!/(?=.*[!#$%&? "])/.test(password)){
+            setInputError('use at least one special charecter')
+            return
+        }
+
 
         createUser(email, password)
         .then((res) =>{
@@ -32,6 +48,14 @@ const Register = () => {
             })
             .catch(error => console.log(error.message))
 
+
+            setInputError('');
+            setInputSucess('Input sucess');
+           
+    
+    
+    
+
       
 
             res.user.displayName = name;
@@ -40,7 +64,10 @@ const Register = () => {
             
             form.reset()
         } )
-        .catch(error => console.log(error.message))
+        .catch(error => {
+            setInputError(error.message)
+            console.log(error.message)
+        })
 
     }
     const handleTasmsPolicy = (event) =>{
@@ -62,6 +89,10 @@ const Register = () => {
                 <input className='block border rounded-lg my-4 p-2 w-72 outline-none' type="text" name="photoURL" id="photoURL" placeholder='Photo URL' />
                 <span className='block my-2 ml-2'><input className='mr-2' type="checkbox" name="checkbox" id="checkbox" onClick={(event)=>handleTasmsPolicy(event)} /><label htmlFor="checkbox">Accept Terms and Policies</label></span>      
                 <input className={!tarmsAndPolices ? `border border-red-600 rounded-lg w-72 py-1 bg-blue-500 hover:bg-white text-white hover:text-blue-500 font-semibold cursor-pointer duration-500` : `border border-blue-600 rounded-lg w-72 py-1 bg-blue-800 hover:bg-white text-white hover:text-blue-800 font-semibold cursor-pointer duration-500` } type="submit" value="Create" disabled={!tarmsAndPolices} />
+                <div>
+                    <h1 className='text-red-400'>{inputError.length ? inputError : ''}</h1>
+                    <h1 className='text-green-600'>{inputSucess.length ? inputSucess : '' }</h1>
+                </div>
                 <p className='my-5'>Already have an account ? <Link to='/login' className='text-blue-600'>Login</Link> </p>
                 <p>
                     <span className='block text-center'>OR</span>
