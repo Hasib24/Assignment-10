f850b835320740dcb8391d11317795a85af33a26
Hasib24 <hasibislam1488@gmail.com>
2023-05-05 12:48:09 +0600

    photoURL fetaure added to the Register.jsx

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -6,6 +6,7 @@ import { getAuth, updateProfile } from 'firebase/auth';
 import { app } from '../../firebase.config';
 import SignInBtns from '../components/SignInBtns';
 
+
 const auth = getAuth(app)
 
 const Register = () => {
@@ -19,12 +20,13 @@ const Register = () => {
         let name = form.name.value;
         let email = form.email.value;
         let password = form.password.value;
+        let photoURL = form.photoURL.value;
 
 
         createUser(email, password)
         .then((res) =>{
 
-            updateProfile(auth.currentUser, { displayName: name })
+            updateProfile(auth.currentUser, { displayName: name, photoURL: photoURL })
             .then(()=>{
                 // profile updated 
             })
@@ -33,7 +35,7 @@ const Register = () => {
       
 
             res.user.displayName = name;
-
+            res.user.photoURL = photoURL;
             setUser(res.user)
             
             form.reset()
@@ -46,17 +48,18 @@ const Register = () => {
     }
 
 
-
     
 
 
 
     return (
         <div className='justify-center flex container mx-auto'>
+            
             <form className='p-10 border' onSubmit={handleOnSubmit}>
                 <input className='block border rounded-lg my-4 p-2 w-72 outline-none' type="text" name="name" id="name" placeholder='Enter name' required />
                 <input className='block border rounded-lg my-4 p-2 w-72 outline-none' type="email" name="email" id="email" placeholder='Enter email' required />
                 <input className='block border rounded-lg my-4 p-2 w-72 outline-none' type="password" name="password" id="password" placeholder='Password' required/>
+                <input className='block border rounded-lg my-4 p-2 w-72 outline-none' type="text" name="photoURL" id="photoURL" placeholder='Photo URL' />
                 <span className='block my-2 ml-2'><input className='mr-2' type="checkbox" name="checkbox" id="checkbox" onClick={(event)=>handleTasmsPolicy(event)} /><label htmlFor="checkbox">Accept Terms and Policies</label></span>      
                 <input className={!tarmsAndPolices ? `border border-red-600 rounded-lg w-72 py-1 bg-blue-500 hover:bg-white text-white hover:text-blue-500 font-semibold cursor-pointer duration-500` : `border border-blue-600 rounded-lg w-72 py-1 bg-blue-800 hover:bg-white text-white hover:text-blue-800 font-semibold cursor-pointer duration-500` } type="submit" value="Create" disabled={!tarmsAndPolices} />
                 <p className='my-5'>Already have an account ? <Link to='/login' className='text-blue-600'>Login</Link> </p>
